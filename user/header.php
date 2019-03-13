5f305410d0f81c629caf0289bfa83e2acda07650
harshu26 <shriharsh26@gmail.com>
2019-03-13 22:58:08 +0530

    First commit

---
+++ user/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>PHP Simple Chat System using AJAX</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	<link rel="stylesheet" href="../css/bootstrap.css" />
+	<script src="../js/bootstrap.js"></script>
+	
+	<link href="../css/dataTables.bootstrap.css" rel="stylesheet">
+	<link href="../css/dataTables.responsive.css" rel="stylesheet">
+
+</head>
